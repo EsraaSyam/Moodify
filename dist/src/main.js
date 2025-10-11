91d39122ae60f2357696ed8e07276451c36f78e7
EsraaSyam <e294014@gmail.com>
2025-10-11 22:45:24 +0300

    fix: adjust main.ts for Vercel compatibility

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,23 +1,43 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const http_exception_filter_1 = require("./common/http-exception.filter");
+const platform_express_1 = require("@nestjs/platform-express");
+const express_1 = __importDefault(require("express"));
 const common_1 = require("@nestjs/common");
-async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+const http_exception_filter_1 = require("./common/http-exception.filter");
+const server = (0, express_1.default)();
+let appInitialized = false;
+async function initApp() {
+    if (appInitialized)
+        return;
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server), {
+        logger: false,
+    });
     app.setGlobalPrefix('api/v1');
     app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
         transform: true,
-        transformOptions: {
-            enableImplicitConversion: true,
-        },
+        transformOptions: { enableImplicitConversion: true },
     }));
     app.enableCors();
-    await app.listen(process.env.PORT ?? 3000);
+    await app.init();
+    appInitialized = true;
 }
-bootstrap();
+async function handler(req, res) {
+    try {
+        await initApp();
+        server(req, res);
+    }
+    catch (err) {
+        console.error('Vercel handler error:', err);
+        res.status(500).send('Internal Server Error');
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { HttpExceptionFilter } from './common/http-exception.filter';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Request, Response } from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+const server = express();
+let appInitialized = false;
+
+async function initApp() {
+  if (appInitialized) return;
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
+    logger: false,
+  });
 
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    transformOptions: {
-      enableImplicitConversion: true, 
-    },
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   app.enableCors();
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+  appInitialized = true;
 }
-bootstrap();
+
+export default async function handler(req: Request, res: Response) {
+  try {
+    await initApp();
+    server(req, res);
+  } catch (err) {
+    console.error('Vercel handler error:', err);
+    res.status(500).send('Internal Server Error');
+  }
+}
